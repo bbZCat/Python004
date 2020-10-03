40614a97b0dbd9111fc2f33620ba54fadeaa1020
ZCat <bbzcat@gmail.com>
2020-10-03 16:28:34 +0800

    V1.1
    1. Update HomeWork02 User-Agent

--- Week02/HomeWork02_2/weblogin.py
+++ Week02/HomeWork02_2/weblogin.py
@@ -10,13 +10,11 @@ passwd = '36540855'
 try:
     #模拟成移动端浏览器，网站第一次登录时要求滑动验证，成功后不再需要
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('user-agent=Mozilla/5.0 (Linux; Android 10; EVR-AL00) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Mobile Safari/537.36')
-    #chrome_options.add_argument('user-agent=Mozilla/5.0 (Linux; Android 5.1; m2 note Build/LMY47D) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/37.0.0.0 Mobile MQQBrowser/6.2 TBS/036215 Safari/537.36 MicroMessenger/6.3.18.800 NetType/WIFI Language/zh_CN')
-    #chrome_options.add_argument('user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 13_1_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.1 Mobile/15E148 Safari/604.1')
+    chrome_options.add_argument('user-agent=Mozilla/5.0 (Linux; Android 5.1; m2 note Build/LMY47D) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/37.0.0.0 Mobile MQQBrowser/6.2 TBS/036215 Safari/537.36 MicroMessenger/6.3.18.800 NetType/WIFI Language/ZH_cn')
     browser = webdriver.Chrome(chrome_options=chrome_options)
 
     browser.get(url)
-    time.sleep(3)
+    time.sleep(2.75)
     
     browser.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div[1]/div/input').send_keys(user)
     browser.find_element_by_xpath('//*[@id="root"]/div/div[2]/div/div/div/div[2]/div/div/div[1]/div[2]/div/input').send_keys(passwd)
